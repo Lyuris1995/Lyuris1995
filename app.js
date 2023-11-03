d5c6918b94a073acfb77db625967df91d250a96d
Lyuris <111432624+Lyuris1995@users.noreply.github.com>
2023-11-03 14:46:53 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -10,8 +10,31 @@ function PageTransitions() {
             let currentBtn = document.querySelectorAll('.active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
             this.className += ' active-btn';
-        })
+        });
     }
+
+    //Sections Active class
+    allSections.forEach(section => {
+        section.addEventListener('click', (e) => {
+            console.log(e.target);
+        const id = e.target.dataset.id;
+        if(id) {
+            //remove selected from the other btns
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active');
+            });
+            e.target.classList.add('active');
+
+            //hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active');
+            });
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+        });
+    });
 }
 
 PageTransitions();
